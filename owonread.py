f1471e2fdee24c79430053ead33dde337727f76f
Robert HH <robert@hammelrath.com>
2015-06-16 22:30:56 +0200

    Added file read options and changed the name

--- owonread.py
+++ owonread.py
@@ -6,13 +6,15 @@ import struct
 import sys
 import socket
 import getopt
+import binascii
 
 def read_data(cmd_txt, parm,  server_addr, fname, skip):
+    res = b""
     try:
         s = socket.socket()
         s.connect(server_addr)
         if s:
-# send command
+# pack & send command
             cmd = cmd_txt + parm
             cmd = struct.pack("<l%ds" % len(cmd), len(cmd), cmd.encode())
             s.send(cmd)
@@ -24,37 +26,61 @@ def read_data(cmd_txt, parm,  server_addr, fname, skip):
             name = s.recv(hdr_size)
             size -= hdr_size
 # open file
-            if fname == '-': 
+            if fname == "":
+                pass
+            elif fname == '-': 
                 f = sys.stdout
             else:
                 f = open(fname, "wb")
 # if additional skip, do it
-            if skip:
+            while skip > 0:
                 img = s.recv(skip)
-                size -= skip
+                size -= len(img)
+                skip -= len(img)
 # get bulk data
-            nrecv = 0
-            while nrecv < size:
-                img = s.recv(size - nrecv)
-                if f == sys.stdout:
-                    f.buffer.write(img)
+            while size > 0:
+                img = s.recv(size)
+                if fname != "":
+                    if f == sys.stdout:
+                        f.buffer.write(img)
+                    else:
+                        f.write(img)
                 else:
-                    f.write(img)
-                nrecv += len(img)
+                    res += img
+                size -= len(img)
 # and that's it
-            if f != sys.stdout:
+            if fname != "" and f != sys.stdout:
                 f.close()
             s.close()
-            return True
+            return res
     except Exception as err:
         sys.stderr.write("Error attempting to get data, reason %s\n" % err)
         return False
 
+def print_dir(s):
+    pos = 0
+    while pos < len(s):
+        if s[pos] == 1:
+            print ("d ", end="")
+        else:   
+            print ("  ", end="")
+        sb = binascii.unhexlify(s[pos+1:pos+9])
+        size = struct.unpack(">l", sb)[0]
+        print ("%8d " % size, end="")
+        pos += 17
+        while s[pos] != ord('|'):
+            print (chr(s[pos]), end="")
+            pos += 1
+        print(" ");
+        pos += 1
+        
+
 def usage():
-    print ("readowon OPTION filename\n" \
+    print ("owonread OPTION filename\n" \
             "   Options: \n"\
-            "   -t type: type for data - image | track | screen\n" \
-            "   -c selection: select channel 1/2/3/4 for track data, jpg/bmp/png for image\n" \
+            "   -t type: type for data - image | track | screen | file\n" \
+            "   -c selection: select channel 1/2/3/4 for track data, \n"\
+            "                 jpg/bmp/png for image, file name for file\n" \
             "   -s # : skip the first # byte from the scope\n" \
             "   -i ip_addr: IP-Address of the oscilloscope\n" \
             "   -p port : port number - default 3000\n" \
@@ -100,6 +126,12 @@ def main():
     elif dtype == 't': # read full track data, to be decoded. Header size is 74 bytes before the raw data
         channel=args_dict['-c'][0]
         read_data("FULLDATA", "CH" + channel, (ipaddr, port), fname, skip)
+    elif dtype == 'f': # read actual screen content, to be decoded. Header size is 278 bytes
+        if args_dict['-c'] == '1': # default
+#            read_data("ROOTDIR", "", (ipaddr, port), "", skip)
+            print_dir(read_data("INNERFILE", "/D", (ipaddr, port), "", skip))
+        else:
+            read_data("INNERFILE", "/D/" + args_dict['-c'], (ipaddr, port), fname, skip)
     else:
         sys.stderr.write ("Unkown data type: %s \n" % args_dict['-t'])
 
--- readme.md
+++ readme.md
@@ -1,27 +1,31 @@
 
+# Owonread: Reading data from the Owon DTS Series oscilloscopes
+
 This is small Python3 script to read the bitmap image, binary image and the 
 full track data from an OWON TDS-Model oscilloscope using LAN. 
 It's quick and dirty, but seems to work. 
 The preferred habitat is Linux, but it should work on OS X and Windows too.
 
-readowon OPTION filename
+Usage:
+'''
+owonread OPTION filename
    Options: 
-   -t type: type for data - image | track | screen
-   -c selection: select channel 1/2/3/4 for track data, jpg/bmp/png for image
+   -t type: type for data - image | track | screen | file
+   -c selection: select channel 1/2/3/4 for track data, 
+                 jpg/bmp/png for image, file name for file
    -s # : skip the first # byte from the scope
    -i ip_addr: IP-Address of the oscilloscope
    -p port : port number - default 3000
    -h print these few help lines
+'''
 If the file name is missing or '-', the data is written to stdout
+
 Default: image = BMP, skip = 0, ip_addr = 'owon.tds', port = 3000, selection = 1/BMP
+
+Reading the file system is pretty basic. If with option -t f no file name is
+given in option -c, the to level directory is displayed. If a file name is
+give in option -c, this file is read.
 
 It's work in progress. Open tasks: 
     - Decode the header coming with full track and binary screen data
-    - reading files from the file system
-
-This morning, Owon supplied a files documenting the communication protocol, just one
-day after I made the first version of this script. That supported me in adding the
-JPG/PNG options.
-
-B.T.W.: You can logon into the TDS-models with telnet and user root w/o password.
 
