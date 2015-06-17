4e667035e129998d1daf3c1fef3aa8c9131874a2
Robert HH <robert@hammelrath.com>
2015-06-17 22:01:34 +0200

    simplified options, dir recurses into subdirs

--- owonread.py
+++ owonread.py
@@ -2,41 +2,46 @@
 #
 # read image or trace from scope
 #
-import struct
 import sys
 import socket
-import getopt
-import binascii
+from getopt import getopt
+from struct import pack, unpack
+from binascii import unhexlify
+import time
 
-def read_data(cmd_txt, parm,  server_addr, fname, skip):
+def read_data(cmd_txt, parm,  server_addr, fname, skip, get):
     res = b""
     try:
         s = socket.socket()
+        s.settimeout(5)
         s.connect(server_addr)
         if s:
 # pack & send command
             cmd = cmd_txt + parm
-            cmd = struct.pack("<l%ds" % len(cmd), len(cmd), cmd.encode())
+            cmd = pack("<l%ds" % len(cmd), len(cmd), cmd.encode())
             s.send(cmd)
 # get length of response
             ss = s.recv(4)
-            size = struct.unpack("<l", ss)[0]
+            size = unpack("<l", ss)[0]
 # skip header
             hdr_size = len(cmd_txt) + 4
             name = s.recv(hdr_size)
             size -= hdr_size
-# open file
-            if fname == "":
-                pass
-            elif fname == '-': 
-                f = sys.stdout
-            else:
-                f = open(fname, "wb")
+# open file, if required
+            if fname != "":
+                if fname != "-":
+                    f = open(fname, "wb")
+                else:
+                    f = sys.stdout
 # if additional skip, do it
             while skip > 0:
                 img = s.recv(skip)
                 size -= len(img)
                 skip -= len(img)
+# check, whether not the full data is required
+            if get > 0 and get < size:
+                skip = size - get
+                size = get
 # get bulk data
             while size > 0:
                 img = s.recv(size)
@@ -48,8 +53,12 @@ def read_data(cmd_txt, parm,  server_addr, fname, skip):
                 else:
                     res += img
                 size -= len(img)
+# eat up what's left                
+            while skip > 0:
+                img = s.recv(skip)
+                skip -= len(img)
 # and that's it
-            if fname != "" and f != sys.stdout:
+            if fname != "" and fname != "-":
                 f.close()
             s.close()
             return res
@@ -57,48 +66,84 @@ def read_data(cmd_txt, parm,  server_addr, fname, skip):
         sys.stderr.write("Error attempting to get data, reason %s\n" % err)
         return False
 
-def print_dir(s):
+# Print a directory file
+def print_dir(d, recurse, ipaddr, port):
+    s = read_data("INNERFILE", d, (ipaddr, port), "", 0, 0)
     pos = 0
     while pos < len(s):
-        if s[pos] == 1:
-            print ("d ", end="")
-        else:   
-            print ("  ", end="")
-        sb = binascii.unhexlify(s[pos+1:pos+9])
-        size = struct.unpack(">l", sb)[0]
-        print ("%8d " % size, end="")
-        pos += 17
-        while s[pos] != ord('|'):
-            print (chr(s[pos]), end="")
+# entry type
+        if s[pos] == 1 and recurse: # recurse into dir
+            print_entry(d, s, pos)  # first print the entry itself
+            newdir = "" # collect name
+            pos += 17
+            while s[pos] != ord('|'):
+                newdir += chr(s[pos])
+                pos += 1
             pos += 1
-        print(" ");
+            if newdir:
+                print_dir(d + "/" + newdir, True, ipaddr, port)
+        else:   # print entry
+            pos = print_entry(d, s, pos)
+
+# print a directory entry at pos
+def print_entry(d, s, pos):
+    if s[pos] == 1:
+        print ("d ", end="")
+    else:
+        print ("- ", end="")
+# file size
+    size = unpack(">l", unhexlify(s[pos+1:pos+9]))[0]
+    print ("%9d " % size, end="")
+    pos += 9
+# date
+    ftime = unpack(">l", unhexlify(s[pos:pos+8]))[0]
+    print (time.strftime("%Y-%m-%d %H:%M:%S ", time.gmtime(ftime)), end="")
+    pos += 8
+# path + name
+    print (d + "/", end="")
+    while s[pos] != ord('|'):
+        print (chr(s[pos]), end="")
         pos += 1
-        
+    print("");
+    pos += 1
+    return pos
 
 def usage():
-    print ("owonread OPTION filename\n" \
-            "   Options: \n"\
-            "   -t type: type for data - image | track | screen | file\n" \
-            "   -c selection: select channel 1/2/3/4 for track data, \n"\
-            "                 jpg/bmp/png for image, file name for file\n" \
-            "   -s # : skip the first # byte from the scope\n" \
-            "   -i ip_addr: IP-Address of the oscilloscope\n" \
-            "   -p port : port number - default 3000\n" \
-            "   -h print these few help lines\n" \
-            "If the file name is missing or '-', the data is written to stdout\n"\
-            "Default: image = BMP, skip = 0, ip_addr = 'owon.tds', port = 3000, selection = 1/BMP")
+    sys.stderr.write ('''
+owonread [OPTIONS] [source] [target]
+
+Options:
+   -t type: data type, as of: bmp jpg png ch1 ch2 ch3 ch4 screen dir file
+      types bmp jpg png get the screen picture
+      types ch1 ch2 ch3 ch4 get the deep trach data of that channel
+      type screen gets the binary screen content
+      type dir shows the content of the devices file store
+      type files get a file. The full path name as shown with dir must
+         be supplied. 
+   -s # : skip the first # bytes of the data from the device
+   -g # : get the following # bytes of the data from the device
+   -i ip_addr: IP-Address of the oscilloscope
+   -p port : port number, default 3000
+   -h print these few help lines
+   
+If the target file name is missing or '-', the data is written to stdout
+Defaults: type = bmp, skip = 0, get = all, ip_addr = 'owon.tds', port = 3000
+''')
 
 
 def main():
 #
 # look for command line arguments
 #
-    args_dict = { '-t' : 'img' , '-c' : '1' , '-s' : '0' , '-i': 'owon-tds', '-p' : '3000', '-h' : 'none'}
+    args_dict = { '-t' : 'BMP' , '-s' : '0' , \
+                  '-i': 'owon-tds', '-p' : '3000', '-g' : '0' , '-h' : 'none'}
 
     try:
-        options, args = getopt.getopt(sys.argv[1:],"ht:c:s:i:p:") # get the options -t xx -c yy -s nn -i ip-addr -p port -h
+        options, args = getopt(sys.argv[1:],"ht:s:i:p:g:") # get the options -t xx -s # -i ip-addr -p port -k # -h
     except: 
+        sys.stderr.write("Option not defined, Usage:\n")
         usage()
+        return
 
     args_dict.update( options ) # Sort the input into the default parameters
 
@@ -111,29 +156,34 @@ def main():
     else:
         fname = "-"
 
+    dtype = args_dict['-t'].upper()
     skip = int(args_dict['-s'])
-    dtype = args_dict['-t'][0].lower()
+    get = int(args_dict['-g'])
     port = int(args_dict['-p'])
     ipaddr = args_dict['-i']
 
-    if dtype == 'i':  # read image
-        if args_dict['-c'].upper() in ("BMP", "JPG", "PNG"):
-            read_data("IMAGE", args_dict['-c'].upper(), (ipaddr, port), fname, skip)
+    if dtype in ("BMP", "JPG", "PNG"):  # read image
+        read_data("IMAGE", dtype, (ipaddr, port), fname, 0, 0)
+    elif dtype == 'SCREEN': # read actual screen content, to be decoded. Header size is 278 bytes
+        read_data("CUTDATA", "", (ipaddr, port), fname, skip, get)
+    elif dtype in ("CH1", "CH2", "CH3", "CH4"): # read full track data, to be decoded. Header size is 74 bytes before the raw data
+        read_data("FULLDATA", dtype, (ipaddr, port), fname, skip, get)
+    elif dtype == 'DIR': # read Directory
+        print("\nDirectory of Files:");
+        print_dir("/D", True, ipaddr, port)
+        print("")
+    elif dtype == 'FILE': # read files from scope
+        if fname != "-":
+            dtype = fname
+            if len(args) > 1:  # additional argument present?
+                fname = args[1] # that must be the filename
+            else:
+                fname = "-"
+            read_data("INNERFILE", dtype, (ipaddr, port), fname, 0, 0)
         else:
-            read_data("IMAGE", "", (ipaddr, port), fname, skip)
-    elif dtype == 's': # read actual screen content, to be decoded. Header size is 278 bytes
-        read_data("CUTDATA", "", (ipaddr, port), fname, skip)
-    elif dtype == 't': # read full track data, to be decoded. Header size is 74 bytes before the raw data
-        channel=args_dict['-c'][0]
-        read_data("FULLDATA", "CH" + channel, (ipaddr, port), fname, skip)
-    elif dtype == 'f': # read actual screen content, to be decoded. Header size is 278 bytes
-        if args_dict['-c'] == '1': # default
-#            read_data("ROOTDIR", "", (ipaddr, port), "", skip)
-            print_dir(read_data("INNERFILE", "/D", (ipaddr, port), "", skip))
-        else:
-            read_data("INNERFILE", "/D/" + args_dict['-c'], (ipaddr, port), fname, skip)
+            sys.stderr.write("Error: Source file name missing\n")
     else:
-        sys.stderr.write ("Unkown data type: %s \n" % args_dict['-t'])
+        sys.stderr.write ("Wrong data type: -t %s\n" % args_dict['-t'])
 
 ##
 ## That's it
--- readme.md
+++ readme.md
@@ -1,31 +1,61 @@
 
-# Owonread: Reading data from the Owon DTS Series oscilloscopes
+#Owonread: Get data from Owon DTS Series oscilloscopes
 
-This is small Python3 script to read the bitmap image, binary image and the 
-full track data from an OWON TDS-Model oscilloscope using LAN. 
-It's quick and dirty, but seems to work. 
-The preferred habitat is Linux, but it should work on OS X and Windows too.
+**Description**
 
-Usage:
-'''
-owonread OPTION filename
-   Options: 
-   -t type: type for data - image | track | screen | file
-   -c selection: select channel 1/2/3/4 for track data, 
-                 jpg/bmp/png for image, file name for file
-   -s # : skip the first # byte from the scope
+This is small Python3 script to read the screen image, binary images, the 
+full track data and files from an OWON TDS-Model oscilloscope using LAN. 
+It's quick and dirty, but seems to work.  The preferred habitat is Linux, 
+but it should work on OS X and Windows too, since no OS-specific syntax is used.
+
+**Usage**
+```
+owonread [OPTIONS] [source] [target]
+
+Options:
+   -t type: data type, as of: bmp jpg png ch1 ch2 ch3 ch4 screen dir file
+      types bmp jpg png get the screen picture
+      types ch1 ch2 ch3 ch4 get the deep trach data of that channel
+      type screen gets the binary screen content
+      type dir shows the content of the devices file store
+      type files get a file. The full path name as shown with dir must
+         be supplied. 
+   -s # : skip the first # bytes of the data from the device
+   -g # : get the following # bytes of the data from the device
    -i ip_addr: IP-Address of the oscilloscope
-   -p port : port number - default 3000
+   -p port : port number, default 3000
    -h print these few help lines
-'''
-If the file name is missing or '-', the data is written to stdout
+   
+If the target file name is missing or '-', the data is written to stdout
+Defaults: type = bmp, skip = 0, get = all, ip_addr = 'owon.tds', port = 3000
+```
 
-Default: image = BMP, skip = 0, ip_addr = 'owon.tds', port = 3000, selection = 1/BMP
+**Examples:**
 
-Reading the file system is pretty basic. If with option -t f no file name is
-given in option -c, the to level directory is displayed. If a file name is
-give in option -c, this file is read.
+`owonread image001.bmp`<br>
+    *get the screen image as BMP file and store it into image001.bmp*
 
-It's work in progress. Open tasks: 
-    - Decode the header coming with full track and binary screen data
+`owonread -t jpg image002.jpg`<br>
+    *get the screen image as BMP file*
+
+`owonread -t ch1 fulldata.bin`<br>
+    *get the full content of CH1 data buffer*
+
+`owonread -t ch3 -s 78 rawdata.bin`<br>
+    *get the full content of CH3 track buffer, skipping the first 78 bytes with header information*
+    
+`owonread -t dir`<br>
+    *Show the list of files stored in the data section of the oscillscope* 
+
+`owonread -t file /D/owon00001.csv pulse_response.csv`<br>
+    *read the file owon0001.csv from the device and store it as pulse_response.csv*
+
+`owonread | display`<br>
+    *get the actual screen image and pipe it into the Linux display command*
+            
+**Notes**
+ 
+- It's work in progress. Open task: Decode the header coming with full track, allowing to send it to e.g. gnuplot. 
+Owon provided some information about the header structure, but it does not fit yet properly.
+- The skip and get options are obviously obsolete, since you could do the same with the Linux **head** command.
 
